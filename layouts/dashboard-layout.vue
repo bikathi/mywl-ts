a21c8daa4f915bc07bc3f6a66ce17578d6997d89
Bikathi Martin <bikathimartin@outlook.com>
2023-11-28 21:20:29 +0300

    Switch dashboard menu entry order

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -77,17 +77,18 @@
 			</nav>
 			<nav class="flex space-x-2 px-2 md:px-48 overflow-x-auto">
 				<NuxtLink
-					:to="{ name: 'user-manager' }"
-					type="button"
-					class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500">
-					User Management
-				</NuxtLink>
-				<NuxtLink
 					:to="{ name: 'issues-home' }"
 					type="button"
 					class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500">
 					Tasks & Issues
 				</NuxtLink>
+				<NuxtLink
+					:to="{ name: 'user-manager' }"
+					type="button"
+					class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500">
+					User Management
+				</NuxtLink>
+
 				<NuxtLink
 					:to="{ name: 'projects-home' }"
 					type="button"
