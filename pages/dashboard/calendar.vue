259858eba245556786c8d7b62dd3dd3543942b46
Bikathi Martin <bikathimartin@outlook.com>
2023-11-30 22:30:30 +0300

    Mount VCalendar for displaying custom calendar

--- pages/dashboard/calendar.vue
+++ pages/dashboard/calendar.vue
@@ -1,5 +1,24 @@
 <template>
-	<h1>your calendar</h1>
+	<main>
+		<div class="flex space-x-6">
+			<div class="w-1/2 p-4">
+				<h1 class="text-2xl font-bold mt-4 mb-2">
+					Calendar At a Glance
+				</h1>
+				<client-only>
+					<VDatePicker
+						v-model="date"
+						:attributes="attrs"
+						expanded
+						:is-dark="true" />
+				</client-only>
+			</div>
+			<div class="w-1/2 p-4">
+				<h1 class="text-2xl font-bold mt-4 mb-2">Upcoming Events</h1>
+			</div>
+		</div>
+		<div></div>
+	</main>
 </template>
 
 <script setup lang="ts">
@@ -7,4 +26,16 @@
 		name: 'user-calendar',
 		layout: 'dashboard-layout',
 	});
+
+	const date: Ref<Date> = ref(new Date());
+	const attrs = ref([
+		{
+			key: 'today',
+			highlight: {
+				color: 'rgb(30 64 175)',
+				fillMode: 'solid',
+			},
+			dates: new Date(),
+		},
+	]);
 </script>
