363e56d28810fbec7f39bb11fd528a3790c087fa
Bikathi Martin <bikathimartin@outlook.com>
2023-12-01 19:29:19 +0300

    Componentized app modal to parent component with slot

---
+++ components/misc/GenericModal.vue
@@ -0,0 +1,10 @@
+<template>
+	<div
+		id="hs-basic-modal"
+		class="hs-overlay hidden w-full h-full fixed top-0 start-0 z-[60] overflow-x-hidden overflow-y-auto pointer-events-none">
+		<div
+			class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
+			<slot />
+		</div>
+	</div>
+</template>
