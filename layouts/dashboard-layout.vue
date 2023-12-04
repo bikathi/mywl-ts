b4f08f7b0c3fe961e3e043b8abfba64f98628b1e
Bikathi Martin <bikathimartin@outlook.com>
2023-12-04 19:06:56 +0300

    Mount componenents for managing notifications

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -114,44 +114,8 @@
 			class="dark:bg-slate-900 dark:border-gray-700 dark:text-white flex-grow px-2 md:px-48">
 			<slot />
 		</div>
-		<!-- offcanvas sidebar for notifications -->
-		<div
-			id="hs-overlay-example"
-			class="hs-overlay hs-overlay-open:translate-x-0 hidden -translate-x-full fixed top-0 start-0 transition-all duration-300 transform rounded-lg h-full max-w-xs w-full z-[60] bg-white border-e dark:bg-gray-800 dark:border-gray-700"
-			tabindex="-1">
-			<div
-				class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
-				<h3 class="font-medium text-lg text-gray-800 dark:text-white">
-					Notifications
-				</h3>
-				<button
-					type="button"
-					class="flex justify-center items-center w-7 h-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-					data-hs-overlay="#hs-overlay-example">
-					<span class="sr-only">Close modal</span>
-					<svg
-						class="flex-shrink-0 w-4 h-4"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<path d="M18 6 6 18" />
-						<path d="m6 6 12 12" />
-					</svg>
-				</button>
-			</div>
-			<div class="p-4">
-				<p class="text-gray-800 dark:text-gray-400">
-					Some text as placeholder. In real life you can have the
-					elements you have chosen. Like, text, images, lists, etc.
-				</p>
-			</div>
-		</div>
+		<!-- for the notification sidebar component -->
+		<NotificationSidebar />
 	</div>
 </template>
 
