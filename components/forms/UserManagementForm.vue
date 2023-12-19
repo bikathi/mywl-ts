fdbe5077eb5df28676492ab45a3a1d14b2163a5a
Bikathi Martin <bikathimartin@outlook.com>
2023-12-19 23:15:58 +0300

    Create form component to manage user details

---
+++ components/forms/UserManagementForm.vue
@@ -0,0 +1,11 @@
+<template>
+	<form
+		action=""
+		method="post"
+		class="flex">
+		<div class="w-1/3 flex-grow"></div>
+		<div class="w-2/3 flex-grow">
+            
+        </div>
+	</form>
+</template>
