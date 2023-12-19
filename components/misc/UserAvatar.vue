97293f44bc45df014d96ca4538fa58d01f189af3
Bikathi Martin <bikathimartin@outlook.com>
2023-12-19 19:54:23 +0300

    Create component to represent user avatar

---
+++ components/misc/UserAvatar.vue
@@ -0,0 +1,16 @@
+<template>
+	<img
+		class="inline-block h-[2.375rem] w-[2.375rem] rounded-full"
+		:src="props.link"
+		alt="Image Description" />
+</template>
+
+<script setup lang="ts">
+	export interface Props {
+		link?: string;
+	}
+
+	const props = withDefaults(defineProps<Props>(), {
+		link: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80',
+	});
+</script>
