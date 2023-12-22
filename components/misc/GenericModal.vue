020903827e280bea6336246a489e3e3d9a76c2b1
Bikathi Martin <bikathimartin@outlook.com>
2023-12-22 17:08:22 +0300

    Modify generic modal height selections

--- components/misc/GenericModal.vue
+++ components/misc/GenericModal.vue
@@ -3,7 +3,7 @@
 		id="hs-basic-modal"
 		class="hs-overlay hidden w-full h-full fixed top-0 start-0 z-[60] overflow-x-hidden overflow-y-auto pointer-events-none">
 		<div
-			class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full m-3 md:mx-auto">
+			class="hs-overlay-open:mt-0 py-2 h-full hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all md:max-w-2xl md:w-full mx-3 md:mx-auto">
 			<slot />
 		</div>
 	</div>
