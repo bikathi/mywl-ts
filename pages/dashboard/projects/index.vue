176048cff75670cb51d16d41ea424c3e88987059
Bikathi Martin <bikathimartin@outlook.com>
2023-12-22 23:23:54 +0300

    Mount project list item renderer for mockup design

--- pages/dashboard/projects/index.vue
+++ pages/dashboard/projects/index.vue
@@ -1,9 +1,15 @@
 <template>
-	<h1>project overview</h1>
+	<div class="h-full w-full bg-green-700 min-h-full py-2 overflow-x-auto">
+		<h1 class="text-xl md:text-3xl">Organization Projects</h1>
+		<ProjectListItem
+			v-for="(item, index) in 4"
+			status="Active" />
+	</div>
 </template>
 
 <script setup lang="ts">
 	definePageMeta({
 		name: 'projects-home',
+		layout: 'dashboard-layout',
 	});
 </script>
