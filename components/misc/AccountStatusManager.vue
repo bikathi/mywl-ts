91e97463f43b2fb17db4cb53781697f1842a4bd9
Bikathi Martin <bikathimartin@outlook.com>
2024-01-06 11:23:18 +0300

    Create component to manage account statuses

---
+++ components/misc/AccountStatusManager.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="text-center mt-2 w-full">
+		<button
+			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700">
+            Activate Account
+        </button>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
