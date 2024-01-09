54432ab18f25e6cfcac168307049557a0a64748c
Bikathi Martin <bikathimartin@outlook.com>
2024-01-09 23:36:25 +0300

    Complete endpoint to update user details

--- components/forms/UpdateDetailsForm.vue
+++ components/forms/UpdateDetailsForm.vue
@@ -1,5 +1,5 @@
 <template>
-	<form>
+	<form @submit.prevent="updateUserDetails">
 		<div class="mb-2">
 			<h1 class="text-2xl font-bold">Account Information</h1>
 			<span
@@ -8,8 +8,7 @@
 					name="ri:information-line"
 					color="currentColor"
 					size="16"
-					class="mr-1" />Information is limited to in-app use
-				only</span
+					class="mr-1" />Effects take place next time you log in</span
 			>
 		</div>
 		<div class="mb-4">
@@ -23,27 +22,104 @@
 				id="user-email"
 				class="form-inputs"
 				placeholder="you@site.com"
-				value="martbikathi@gmail.com" />
+				v-model="email" />
 		</div>
 		<div class="my-4">
 			<label
-				for="user-name"
+				for="first-name"
+				class="block font-medium mb-2 dark:text-white"
+				>First Name</label
+			>
+			<input
+				type="text"
+				id="first-name"
+				class="form-inputs"
+				placeholder="John"
+				v-model="firstName" />
+		</div>
+		<div class="my-4">
+			<label
+				for="other-name"
+				class="block font-medium mb-2 dark:text-white"
+				>Other Name</label
+			>
+			<input
+				type="text"
+				id="other-name"
+				class="form-inputs"
+				placeholder="Doe"
+				v-model="otherName" />
+		</div>
+		<div class="my-4">
+			<label
+				for="username"
 				class="block font-medium mb-2 dark:text-white"
 				>Username</label
 			>
 			<input
 				type="text"
-				id="user-name"
+				id="username"
 				class="form-inputs"
-				placeholder="you@site.com"
-				value="Bikathi Martin" />
+				placeholder="Doe"
+				v-model="username" />
 		</div>
 		<div>
 			<button
-				type="button"
+				type="submit"
 				class="py-3 px-4 text-sm inline-flex items-center font-semibold rounded-md bg-blue-600 text-white hover:bg-blue-700">
 				Update Changes
 			</button>
 		</div>
 	</form>
 </template>
+
+<script setup lang="ts">
+	import { usePrincipal } from '~/stores/usePrincipal';
+	const { getDetails } = usePrincipal();
+	const email: Ref<string> = ref('');
+	const firstName = ref('');
+	const otherName = ref('');
+	const username = ref('');
+	const loadingUpdateDetails: Ref<boolean> = ref(false);
+	const { openToast } = useToast();
+
+	async function updateUserDetails() {
+		loadingUpdateDetails.value = true;
+		await useFetch('/api/v1/accounts/update-account', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				userId: getDetails.userId,
+				firstName: firstName.value,
+				otherName: otherName.value,
+				username: username.value,
+				email: getDetails.email,
+				dateOfBirth: getDetails.dateOfBirth,
+				roles: getDetails.roles,
+				department: getDetails.department,
+			}),
+			async onRequestError() {
+				// TODO: Fixup issues with alert box
+				loadingUpdateDetails.value = false;
+				openToast('Something went wrong. Please try again.', 'danger');
+			},
+			async onResponse({ response }) {
+				loadingUpdateDetails.value = false;
+				const responseData = response._data;
+				if (response.status === 200) {
+					openToast(responseData.message, 'info');
+				}
+			},
+		});
+	}
+
+	onBeforeMount(() => {
+		email.value = getDetails.email;
+		firstName.value = getDetails.firstName;
+		otherName.value = getDetails.otherName;
+		username.value = getDetails.username;
+	});
+</script>
