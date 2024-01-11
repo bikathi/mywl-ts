d922071386032cc0378c46a7c9ed89e92b5ef658
Bikathi Martin <bikathimartin@outlook.com>
2024-01-11 23:30:59 +0300

    Conditionally render proper button based on accountEnabled variable

--- components/misc/AccountStatusManager.vue
+++ components/misc/AccountStatusManager.vue
@@ -1,10 +1,22 @@
 <template>
 	<div class="text-center mt-2 w-full">
 		<button
-			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700">
-            Activate Account
-        </button>
+			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-red-600 text-white hover:bg-red-700"
+			v-if="props.accountEnabled">
+			Deactivate Account
+		</button>
+		<button
+			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-green-600 text-white hover:bg-green-700"
+			v-else-if="!props.accountEnabled">
+			Activate Account
+		</button>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	export interface Props {
+		accountEnabled: boolean;
+	}
+
+	const props = defineProps<Props>();
+</script>
