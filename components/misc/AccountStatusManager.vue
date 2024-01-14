09b0a5e9045435ba723c51f2a242a4f5fde9cab2
Bikathi Martin <bikathimartin@outlook.com>
2024-01-14 21:32:24 +0300

    Improve on use of  calls

--- components/misc/AccountStatusManager.vue
+++ components/misc/AccountStatusManager.vue
@@ -3,13 +3,13 @@
 		<button
 			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-red-600 text-white hover:bg-red-700"
 			v-if="props.accountEnabled"
-			@click="deactivateAccount">
+			@click="modifyAccountStatus('deactivate')">
 			Deactivate Account
 		</button>
 		<button
 			class="py-2 w-[45%] px-3 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-green-600 text-white hover:bg-green-700"
 			v-else-if="!props.accountEnabled"
-			@click="activateAccount">
+			@click="modifyAccountStatus('activate')">
 			Activate Account
 		</button>
 	</div>
@@ -24,8 +24,8 @@
 	const props = defineProps<Props>();
 	const { openToast } = useToast();
 
-	async function deactivateAccount() {
-		await useFetch('/api/v1/accounts/deactivate', {
+	async function modifyAccountStatus(endpointStatus: string) {
+		await useFetch(`/api/v1/accounts/${endpointStatus}`, {
 			method: 'PATCH',
 			headers: {
 				Accept: 'application/json',
@@ -37,26 +37,7 @@
 
 			async onResponse({ response }) {
 				if (response.status === 200) {
-					openToast('User account has been deactivated.', 'info');
-					setTimeout(() => location.reload(), 2000);
-				}
-			},
-		});
-	}
-
-	async function activateAccount() {
-		await useFetch('/api/v1/accounts/activate', {
-			method: 'PATCH',
-			headers: {
-				Accept: 'application/json',
-			},
-			body: JSON.stringify({
-				userId: props.userId,
-			}),
-
-			async onResponse({ response }) {
-				if (response.status === 200) {
-					openToast('User account has been activated.', 'info');
+					openToast('Account status successfully modified.', 'info');
 					setTimeout(() => location.reload(), 2000);
 				}
 			},
