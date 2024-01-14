80c54ae8ab052f727fad779531d618d832cc72ac
Bikathi Martin <bikathimartin@outlook.com>
2024-01-14 20:25:42 +0300

    Reduce buggy code in Ofetch interceptor

--- plugins/ofetch-interceptor.client.ts
+++ plugins/ofetch-interceptor.client.ts
@@ -3,7 +3,6 @@ import { ofetch } from "ofetch";
 export default defineNuxtPlugin((_nuxtApp) => {
     const authToken = useCookie('AUTH-TOKEN');
 	const csrfToken = useCookie('CSRF-TOKEN');
-    const { openToast } = useToast();
     const allowedNonCSRFMethods: readonly String[] = ["GET", "HEAD", "TRACE", "OPTIONS"];
 
     globalThis.$fetch = ofetch.create({
@@ -20,9 +19,6 @@ export default defineNuxtPlugin((_nuxtApp) => {
             }
 
             options.headers = {...options.headers, ...headers};
-        },
-        async onRequestError() {
-            openToast('Something went wrong. Please try again.', 'danger');
         },
     });
 });
