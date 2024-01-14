ba6db745efc7f6d97be5bae9d60f1a3facd22f96
Bikathi Martin <bikathimartin@outlook.com>
2024-01-14 22:23:38 +0300

    Improve code for API requests interceptor

--- plugins/ofetch-interceptor.client.ts
+++ plugins/ofetch-interceptor.client.ts
@@ -4,6 +4,7 @@ export default defineNuxtPlugin((_nuxtApp) => {
     const authToken = useCookie('AUTH-TOKEN');
 	const csrfToken = useCookie('CSRF-TOKEN');
     const allowedNonCSRFMethods: readonly String[] = ["GET", "HEAD", "TRACE", "OPTIONS"];
+    const { openToast } = useToast();
 
     globalThis.$fetch = ofetch.create({
         async onRequest({ request, options }) {
@@ -20,5 +21,24 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
             options.headers = {...options.headers, ...headers};
         },
+
+        async onResponseError({ response }) {
+            const responseBody = response._data;
+            console.log("inside request interceptor: responseBody: ", responseBody);
+            switch(responseBody.status) {
+                case 401: {
+                    openToast(responseBody.message, 'danger');
+                    break;
+                }
+
+                case 500: {
+                    openToast(responseBody.message, 'waring');
+                }
+
+                case 404: {
+                    openToast(responseBody.message, 'waring');
+                }
+            }
+        }
     });
 });
