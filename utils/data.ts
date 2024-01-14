00eb63b8df6b960eb9c39a7207ac435b72fc50a8
Bikathi Martin <bikathimartin@outlook.com>
2024-01-14 21:49:07 +0300

    Declare re-usable application data

---
+++ utils/data.ts
@@ -0,0 +1,18 @@
+export const availableRoles: readonly object[] = [
+    {
+        text: 'Is A Moderator',
+        role: 'role_moderator',
+        id: 'mod_role',
+    },
+    {
+        text: 'Is An Admin',
+        role: 'role_admin',
+        id: 'admin_role',
+    },
+]
+
+export const availableDepartments: readonly string[] = [
+    'IT Department',
+    'Customer Care Department',
+    'Field Technician',
+];
