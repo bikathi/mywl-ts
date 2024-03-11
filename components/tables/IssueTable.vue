dee59a63785985e387434e09665048c25b801e0f
Bikathi Martin <bikathimartin@outlook.com>
2024-03-11 22:38:32 +0300

    Modify UI to allow proper passing of props

--- components/tables/IssueTable.vue
+++ components/tables/IssueTable.vue
@@ -1,100 +1,55 @@
 <template>
-	<div class="flex flex-col">
-		<div class="-m-1.5 overflow-x-auto">
-			<div class="p-1.5 min-w-full inline-block align-middle">
-				<div
-					class="border rounded-lg overflow-hidden dark:border-gray-700">
-					<table
-						class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-						<thead class="bg-gray-50 dark:bg-gray-700">
-							<tr>
-								<th
-									scope="col"
-									class="issue-table-headers">
-									Title
-								</th>
-								<th
-									scope="col"
-									class="issue-table-headers">
-									Technician / Handler
-								</th>
-								<th
-									scope="col"
-									class="issue-table-headers">
-									Status
-								</th>
-								<th
-									scope="col"
-									class="issue-table-headers">
-									Opened By
-								</th>
-							</tr>
-						</thead>
-						<tbody
-							class="divide-y divide-gray-200 dark:divide-gray-700">
-							<tr
-								v-for="a in 5"
-								class="group">
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200 inline-flex items-center space-x-2">
-									<span
-										v-html="svg"
-										class="w-8 h-8 rounded-full"></span>
-									<span class="inline-flex flex-col">
-										<NuxtLink
-											class="hover:underline underline-offset-2 group-hover:underline"
-											:to="{
-												name: 'issue-viewer',
-												params: {
-													id: id,
-												},
-											}"
-											>{{ title }}</NuxtLink
-										>
-										<span
-											class="text-xs font-bold text-gray-500 uppercase"
-											>by {{ clientName }}</span
-										>
-									</span>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-									{{ assignee }}
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 inline-flex items-center space-x-2">
-									<span
-										class="w-3 h-3 block rounded-full bg-orange-400"
-										v-if="
-											issueStatus === 'Inactive'
-										"></span>
-									<span
-										class="w-3 h-3 block rounded-full bg-green-400"
-										v-else-if="
-											issueStatus === 'Active'
-										"></span>
-									<span
-										class="w-3 h-3 block rounded-full bg-purple-400"
-										v-else></span>
-									<span>{{ issueStatus }}</span>
-								</td>
-								<td
-									class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-									<span class="inline-flex flex-col">
-										<span>{{ openedBy }}</span>
-										<span
-											class="text-xs font-medium lowercase text-gray-500"
-											>on {{ date }}</span
-										>
-									</span>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
+	<tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+		<tr class="group">
+			<td
+				class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200 inline-flex items-center space-x-2">
+				<span
+					v-html="svg"
+					class="w-8 h-8 rounded-full"></span>
+				<span class="inline-flex flex-col">
+					<NuxtLink
+						class="hover:underline underline-offset-2 group-hover:underline"
+						:to="{
+							name: 'issue-viewer',
+							params: {
+								id: id,
+							},
+						}"
+						>{{ title }}</NuxtLink
+					>
+					<span class="text-xs font-bold text-gray-500 uppercase"
+						>by {{ clientName }}</span
+					>
+				</span>
+			</td>
+			<td
+				class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+				{{ technicianA }}, {{ technicianB }}
+			</td>
+			<td
+				class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 inline-flex items-center space-x-2">
+				<span
+					class="w-3 h-3 block rounded-full bg-orange-400"
+					v-if="issueStatus === 'Inactive'"></span>
+				<span
+					class="w-3 h-3 block rounded-full bg-green-400"
+					v-else-if="issueStatus === 'Active'"></span>
+				<span
+					class="w-3 h-3 block rounded-full bg-purple-400"
+					v-else></span>
+				<span>{{ issueStatus }}</span>
+			</td>
+			<td
+				class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+				<span class="inline-flex flex-col">
+					<span>{{ openedBy }}</span>
+					<span class="text-xs font-medium lowercase text-gray-500"
+						>on {{ date }}</span
+					>
+				</span>
+			</td>
+		</tr>
+	</tbody>
 </template>
 
 <script setup lang="ts">
@@ -103,7 +58,8 @@
 	export interface Props {
 		title: string;
 		clientName: string;
-		assignee: string;
+		technicianA: string;
+		technicianB: string;
 		status: string; // active, inactive, closed
 		id: string;
 		date: string;
