5ff196a2d1258557a9bb10d0795c32df69a633ed
Bikathi Martin <bikathimartin@outlook.com>
2024-03-18 22:01:48 +0300

    Hide the notification icon

--- layouts/dashboard-layout.vue
+++ layouts/dashboard-layout.vue
@@ -7,7 +7,7 @@
 				class="flex items-center justify-between px-2 lg:px-56 h-full py-2">
 				<h1 class="text-2xl lg:text-3xl font-bold">Poa Internet</h1>
 				<div class="hs-dropdown inline-flex items-center space-x-2">
-					<button
+					<!-- <button
 						type="button"
 						class="p-1 dark:text-white"
 						data-hs-overlay="#hs-overlay-example">
@@ -16,7 +16,7 @@
 							color="currentColor"
 							size="23"
 							class="" />
-					</button>
+					</button> -->
 					<button
 						id="hs-dropdown-with-icons"
 						type="button"
@@ -87,7 +87,7 @@
 					User Management
 				</NuxtLink>
 
-				<NuxtLink
+				<!-- <NuxtLink
 					:to="{ name: 'projects-home' }"
 					type="button"
 					class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500">
@@ -98,7 +98,7 @@
 					type="button"
 					class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500">
 					Assignments & Calendar
-				</NuxtLink>
+				</NuxtLink> -->
 			</nav>
 		</div>
 		<div
